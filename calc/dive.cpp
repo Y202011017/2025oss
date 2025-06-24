0b3b1230949863bd166654062ef368bde78a3292
KHS <kim52885287@gmail.com>
2025-06-24 15:25:08 +0900

    [*]dive.cpp dev/c 에서 수정함

--- calc/dive.cpp
+++ calc/dive.cpp
@@ -1,8 +1,14 @@
-#include "dive.h"
+#include "sub.h"
 
 #include <stdio.h>
 
 int dive(int x, int y)
 {
-	return 0;
+	int cnt = 0;
+	while (x > y)
+	{ 
+		cnt++;
+		x = sub(x,y);
+	}
+	return cnt;
 }
